a4d281a7e9e1871d8c834aa4ef3b9b5c28b05048
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-12-21 13:41:02 +0300

    25 - Password Change action, view ve view model'i oluşturuldu. Kullanıcı şifresini değiştirebiliyor.

--- Identity/Controllers/MemberController.cs
+++ Identity/Controllers/MemberController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Identity.Models;
 using Identity.ViewModels;
@@ -28,6 +29,42 @@ namespace Identity.Controllers
             var model = user.Adapt<UserViewModel>();
 
             return View(model);
+        }
+
+        public IActionResult PasswordChange()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PasswordChange(ChangePasswordViewModel model)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (user is null)
+                return BadRequest("Kullanıcı bulunamadı");
+
+            var isPasswordCorrect = await _userManager.CheckPasswordAsync(user, model.Password);
+            if (!isPasswordCorrect)
+                return BadRequest("Şifreniz yanlış");
+
+            if (model.NewPassword != model.NewPasswordConfirm)
+                return BadRequest("Yeni şifreniz uyuşmuyor");
+
+            var result = await _userManager.ChangePasswordAsync(user, model.Password, model.NewPassword);
+            if (result.Succeeded)
+            {
+                await _userManager.UpdateSecurityStampAsync(user);
+
+                await _signInManager.SignOutAsync();
+                await _signInManager.PasswordSignInAsync(user, model.NewPassword, true, false);
+
+                return Redirect("Index");
+            }
+            else
+                foreach (var error in result.Errors)
+                    Console.WriteLine(error);
+
+            return View();
         }
     }
 }
---
+++ Identity/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,9 @@
+namespace Identity.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        public string Password { get; set; }
+        public string NewPassword { get; set; }
+        public string NewPasswordConfirm { get; set; }
+    }
+}
