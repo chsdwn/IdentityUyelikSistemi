bcdefd8fcdf3558d5ceaa947573f9da5a7479db7
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-12-21 13:14:02 +0300

    24 - Member sayfasına kullanıcı bilgileri ve menü eklendi.

--- Identity/Controllers/MemberController.cs
+++ Identity/Controllers/MemberController.cs
@@ -1,4 +1,9 @@
+using System.Threading.Tasks;
+using Identity.Models;
+using Identity.ViewModels;
+using Mapster;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Identity.Controllers
@@ -6,9 +11,23 @@ namespace Identity.Controllers
     [Authorize]
     public class MemberController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public MemberController(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager)
         {
-            return View();
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var model = user.Adapt<UserViewModel>();
+
+            return View(model);
         }
     }
 }
